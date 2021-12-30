e3328027ec3a9dfc9752709b6251ee09893d005d
marlon Felipe Passos <1marlonpassos@gmail.com>
2021-12-29 23:18:24 -0300

    add reset css

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header/Header'
+import "the-new-css-reset/css/reset.css"
+
+const Home: NextPage = () => {
+  return (
+    <>  
+      <Header/>
+
+    </>
+  )
+}
+
+export default Home
