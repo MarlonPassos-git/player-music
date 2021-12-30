635937c473c94e5a4091ba3a44472d14581f19b5
marlon Felipe Passos <1marlonpassos@gmail.com>
2021-12-29 23:47:10 -0300

    add logo style

---
+++ src/components/Header/styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { progressiveClamp } from 'progressive-size'
+
+export const Container = styled.header`
+    display: flex;
+    align-items: center;
+    height: ${progressiveClamp(84, 140)};
+    padding: 0 var(--spacing-padding);
+
+`
+
+export const Logo = styled.img`
+    height: ${progressiveClamp(30, 50)};
+`
