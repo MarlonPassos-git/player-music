e461811df22e84e742dd5f8bf41029719ccf6260
marlon Felipe Passos <1marlonpassos@gmail.com>
2021-12-30 01:08:13 -0300

    add global style in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header/Header'
 import "the-new-css-reset/css/reset.css"
+import { GlobalStyle } from '../styles/GlobalStyle'
 
 const Home: NextPage = () => {
   return (
     <>  
       <Header/>
-
+      <GlobalStyle />
     </>
   )
 }
