2f478640114aa9b2e13c0123de73d39fcd964fcf
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-02 11:41:38 -0300

    add SearchResult context  on page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from 'next/app'; 
-
+import { SearchResultProvider } from '../context/SearchResult';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <SearchResultProvider>
+        <Component {...pageProps} />
+      </SearchResultProvider>
       
-      <Component {...pageProps} />
     </>  
   )
   
