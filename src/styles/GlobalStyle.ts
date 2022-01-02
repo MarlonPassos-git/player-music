eb670ed932affc34ebaedc487b5b6d1119410368
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-02 01:55:10 -0300

    add standard font and color

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -21,6 +21,10 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
       background-color: var(--white);
+      font-family: 'Oswald', sans-serif;
   }
 
+  h1,h2,h3,h4,h5,h6,p {
+    color: var(--black);
+  }
 `
