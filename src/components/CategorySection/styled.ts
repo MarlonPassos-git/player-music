afb565d2e2a9f67d593620350c6586b0394109b9
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-02 01:57:26 -0300

    create style of categorySection

---
+++ src/components/CategorySection/styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+import _Carousel  from 'react-elastic-carousel'
+
+export const Title = styled.h2`
+   
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 36px;
+    margin-bottom: 19px;
+    
+
+`
+
+export const Carousel = styled(_Carousel)`
+    user-select: none !important;
+
+   
+`
