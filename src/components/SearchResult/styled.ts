66431645daa0dfd7adf52b7d17e09004fc9b1922
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-02 22:19:00 -0300

    Create SearchResult styled

---
+++ src/components/SearchResult/styled.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Contatainer = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 25px;
+    justify-content: center;
+
+
+`
+
+export const WrapperButton = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+`
+
+export const Button = styled.button`
+
+
+`
