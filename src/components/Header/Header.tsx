079b8605c56039e7dc81f9ae16b3a17329858c84
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-04 13:00:02 -0300

    add favorite button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,22 +3,30 @@ import { Container } from './styled'
 import { Logo } from './styled'
 import SearchBar from '../SearchBar/SearchBar'
 import Link from 'next/link'
+import { FavoriteButton } from './styled'
+import { FaHeart } from 'react-icons/fa'
 
 
 export default function Header() {
     return (
         <Container>
-            <Link href="/">
-                <a>
-                    <Logo
-                        src='/logo.svg'
-                        alt="OuveAe Logo"
-                    />
-                </a>
+            
+            <Logo
+                src='/logo.svg'
+                alt="OuveAe Logo"
+            />
                 
-            </Link>
+                
+            
             
             <SearchBar />
+            <FavoriteButton >
+                <FaHeart
+                    size={24}
+                    color="var(--strong_cyan)"
+                />
+
+            </FavoriteButton>
             
             
             
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -15,9 +15,46 @@ export const Container = styled.header`
     top: 0;
     z-index: 100;
 
+    @media (max-width: 550px) {
+        display: grid;
+        grid-template: 
+            "logo logo"
+            "searchBar favoriteButton"
+            / 1fr 44px;
+        ;
+        height: ${Rem(154)};
+    }
 
 `
 
 export const Logo = styled.img`
     height: ${Rem(44)};
+    grid-area: logo;
+    margin-right: ${Rem(20)};
+
+    @media (max-width: 550px) {
+        justify-self: center;
+
+    }
+`
+
+export const FavoriteButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 2px 5px 1px rgb(64 60 67 / 16%);
+    cursor: pointer;
+    min-height: ${Rem(44)};
+    min-width: ${Rem(44)};
+    border-radius: 50%;
+    transition: all 0.15s;
+    grid-area: favoriteButton;
+    
+    &:hover {
+
+        svg {
+            transform: scale3d(1.1, 1.1, 1.1);
+        }
+    }
+
 `
