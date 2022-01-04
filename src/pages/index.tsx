63e6b28dce5231ce13097c833061ffa3f677dde4
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-04 02:00:51 -0300

    add musicPlayer Component to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import Header from '../components/Header/Header'
 import "the-new-css-reset/css/reset.css"
 import { GlobalStyle } from '../styles/GlobalStyle'
 import { Main } from '../components/Main/Main'
+import { MusicPlayer } from '../components/MusicPlayer/MusicPlayer'
 
 const Home: NextPage = () => {
   return (
     <>  
       <Header />
       <Main />
+      <MusicPlayer />
       <GlobalStyle />
     </>
   )
